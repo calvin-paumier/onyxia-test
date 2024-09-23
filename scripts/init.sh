e8c138ab107f3cf836802e1ca20207a35e62bb95
Calvin Paumier <48155703+calvin-paumier@users.noreply.github.com>
2024-09-23 13:12:10 +0200

    fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,10 +8,10 @@ REPO_URL="https://github.com/calvin-paumier/onyxia-test"
 git clone --depth 1 $REPO_URL $CLONE_DIR
 
 # Put relevant notebook in formation dir
-cp "notebooks/hello_world.ipynb" "${WORK_DIR}"
+cp "${CLONE_DIR}/notebooks/hello_world.ipynb" "${WORK_DIR}"
 
 # Remove useless repository
 rm -rf $CLONE_DIR
 
 # Open the relevant notebook when starting Jupyter Lab
-echo "c.LabApp.default_url = '/notebooks/hello_world.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
+echo "c.LabApp.default_url = '/lab/tree/hello_world.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
