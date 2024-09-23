f89b6679d43ee0ad68bbb14d6632746f797a50e6
Calvin Paumier <48155703+calvin-paumier@users.noreply.github.com>
2024-09-23 11:05:12 +0200

    test

---
+++ scripts/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WORK_DIR="/home/onyxia/work"
+CLONE_DIR="${WORK_DIR}/repo-git"
+
+# Clone course repository
+REPO_URL="https://github.com/calvin-paumier/onyxia-test"
+git clone --depth 1 $REPO_URL $CLONE_DIR
+
+# Put relevant notebook in formation dir
+cp "scripts/init.ipynb" "${WORK_DIR}"
+
+# Remove useless repository
+rm -rf $CLONE_DIR
+
+# Open the relevant notebook when starting Jupyter Lab
+echo "c.LabApp.default_url = '/notebooks/hello_world.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
