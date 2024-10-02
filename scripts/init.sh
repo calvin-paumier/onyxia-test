5486e1853d781e5fbbe5ec980082a6021660c5d1
Calvin Paumier <48155703+calvin-paumier@users.noreply.github.com>
2024-10-02 10:05:33 +0200

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,11 +12,11 @@ cp "${CLONE_DIR}/notebooks/llm_test.ipynb" "${WORK_DIR}"
 cp "${CLONE_DIR}/notebooks/hello_world.ipynb" "${WORK_DIR}"
 cp "${CLONE_DIR}/docs/solfege.txt" "${WORK_DIR}"
 
-# Remove useless repository
-rm -rf $CLONE_DIR
-
 # requierements
 pip install -r "${CLONE_DIR}/requirements.txt"
+
+# Remove useless repository
+rm -rf $CLONE_DIR
 
 # Open the relevant notebook when starting Jupyter Lab
 echo "c.LabApp.default_url = '/lab/tree/llm_test.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
